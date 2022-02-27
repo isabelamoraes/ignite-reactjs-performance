0884e4d4a49d0fc338421a261c89f76744f67d3d
Isabela Moraes <isabelagmoraes@hotmail.com>
2022-02-27 18:30:37 -0300

    Upload to git

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center">
+  Performance
+</h1>
+
+<h4 align="center">
+  Examples that help improve application performance
+</h4>
+
+<p align="center">
+  <a href="#features">Features</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#getting-started">Getting Started</a>&nbsp;&nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;
+  <a href="#reference">Reference</a>
+</p>
+
+## Features
+
+This project was developed with the following technologies:
+
+- **[Next.js](https://nextjs.org/)** - A framework for creating React applications.
+
+## Getting Started
+
+To clone and run this application, place from your command line:
+
+```bash
+# Clone this repository
+$ git clone https://github.com/isabelamoraes/ignite-reactjs-performance.git
+
+# Go into the repository
+$ cd ignite-reactjs-performance
+
+# Install dependencies
+$ yarn
+
+# Run the server
+$ yarn server
+
+# Run the application
+$ yarn dev
+
+```
+
+## Reference
+
+This application was developed during the classes of the Chapter V of the ReactJS track on Ignite, training promoted by Rocketseat.
---
+++ components/ProductItem.tsx
@@ -0,0 +1,50 @@
+import { memo, useState } from "react";
+import dynamic from "next/dynamic"; //lazy no React
+import { AddProductToWishListProps } from "./AddProductToWishList";
+//import { AddProductToWishList } from "./AddProductToWishlist";
+
+const AddProductToWishList = dynamic<AddProductToWishListProps>(
+  () => {
+    return import("./AddProductToWishList").then(
+      (mod) => mod.AddProductToWishList
+    ); //Exportação default não precisaria do then
+  },
+  {
+    loading: () => <span>Carregando...</span>,
+  }
+);
+
+interface ProductItemProps {
+  product: {
+    id: number;
+    priceFormatted: string;
+    title: string;
+  };
+  onAddToWishList: (id: number) => void;
+}
+
+function ProductItemComponent({ product, onAddToWishList }: ProductItemProps) {
+  const [isAddingToWishList, setIsAddingToWishList] = useState(false);
+
+  return (
+    <div>
+      {product.title} - <strong>R$ {product.priceFormatted} </strong>
+      <button onClick={() => setIsAddingToWishList(true)}>
+        Adicionar aos favoritos
+      </button>
+      {isAddingToWishList && (
+        <AddProductToWishList
+          onAddToWishList={() => onAddToWishList(product.id)}
+          onRequestClose={() => setIsAddingToWishList(false)}
+        />
+      )}
+    </div>
+  );
+}
+
+export const ProductItem = memo(
+  ProductItemComponent,
+  (prevProps, nextProps) => {
+    return Object.is(prevProps.product, nextProps.product);
+  }
+);
